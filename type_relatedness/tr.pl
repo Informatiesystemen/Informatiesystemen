f2bcba6864fbee0b67c9c5fdf5248a5c7bacb0c5
Hendrikto <hendrik.to@gmail.com>
2017-12-10 15:28:50 +0100

    Update examples with new query syntax.

--- type_relatedness/tr.pl
+++ type_relatedness/tr.pl
@@ -3,27 +3,27 @@
 % Examples
 
 % T1
-% ?- related(a, a).
+% ?- type_related(a, a).
 
 % T2
 %% related(a, b).
-% ?- related(b, a).
+% ?- type_related(b, a).
 
 % T3
 %% pater_familias(z, y).
 %% pater_familias(x, y).
 %% related(z, y).
-% ?- related(z, x).
+% ?- type_related(z, x).
 
 % T4
 %% gen(y, x).
 %% related(x, z).
-% ?- related(y, z).
+% ?- type_related(y, z).
 
 % T5 & T6
 %% element_type(x, z).
 %% element_type(y, z).
-% ?- related(x, y).
+% ?- type_related(x, y).
 
 % T2
 type_related(X, Y) :- related(X, Y); related(Y, X).
