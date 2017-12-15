64ad2efc99018033476d2bf6316fb1497798db11
Hendrikto <hendrik.to@gmail.com>
2017-12-15 13:57:46 +0100

    Impose an ordering on object types, to solve infinite recursion better.

--- type_relatedness/tr.pl
+++ type_relatedness/tr.pl
@@ -3,36 +3,36 @@
 % Examples
 
 % T1
-% ?- type_related(a, a).
+% ?- related(a, a).
 
 % T2
 %% related(a, b).
-% ?- type_related(b, a).
+% ?- related(b, a).
 
 % T3
 %% pater_familias(z, y).
 %% pater_familias(x, y).
-%% related(z, y).
-% ?- type_related(z, x).
+%% related(y, z).
+% ?- related(z, x).
 
 % T4
 %% gen(y, x).
 %% related(x, z).
-% ?- type_related(y, z).
+% ?- related(y, z).
 
 % T5
 %% group_type(x).
 %% group_type(y).
 %% element_type(x, z).
 %% element_type(y, z).
-% ?- type_related(x, y).
+% ?- related(x, y).
 
 % T6
 %% seqence_type(x).
 %% seqence_type(y).
 %% element_type(x, z).
 %% element_type(y, z).
-% ?- type_related(x, y).
+% ?- related(x, y).
 
 % Figure 2.26
 %% element_type(b, a).
@@ -43,26 +43,31 @@
 %% pater_familias(d, a).
 
 related_types(X, RelatedTypes) :-
-	findall(Y, type_related(X, Y), RT),
+	findall(Y, related(X, Y), RT),
 	sort(RT, RelatedTypes). % sort/2 also removes duplicates
-
-% T2
-type_related(X, Y) :- related(X, Y); related(Y, X).
 
 % T1
 related(X, X).
+
+% T2
+related(X, Y) :-
+	atom(X), atom(Y),
+	atom_string(X, XS),
+	atom_string(Y, YS),
+	XS > YS,
+	related(Y, X).
 
 % T3
 related(X, Z) :-
 	pater_familias(X, Top),
 	pater_familias(Y, Top),
 	X \= Y,
-	type_related(Y, Z).
+	related(Y, Z).
 
 % T4
 related(X, Z) :-
 	gen(X, Y),
-	type_related(Y, Z).
+	related(Y, Z).
 
 % T5
 related(X, Y) :-
@@ -70,7 +75,7 @@ related(X, Y) :-
 	group_type(Y),
 	element_type(X, T1),
 	element_type(Y, T2),
-	type_related(T1, T2).
+	related(T1, T2).
 
 % T6
 related(X, Y) :-
@@ -78,7 +83,7 @@ related(X, Y) :-
 	seqence_type(Y),
 	element_type(X, T1),
 	element_type(Y, T2),
-	type_related(T1, T2).
+	related(T1, T2).
 
 % default values
 element_type(_, _) :- false.
